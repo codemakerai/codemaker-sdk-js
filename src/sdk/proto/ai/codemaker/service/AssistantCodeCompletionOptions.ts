b4d1a4ecb189d51213f97a2a463577aefb7c7fc8
codemakerai-dev <codemaker-dev@codemaker.ai>
2024-02-01 16:44:54 -0800

    Release v2.6.0

---
+++ src/sdk/proto/ai/codemaker/service/AssistantCodeCompletionOptions.ts
@@ -0,0 +1,10 @@
+// Original file: src/sdk/proto/codemakerai.proto
+
+
+export interface AssistantCodeCompletionOptions {
+  'contextId'?: (string);
+}
+
+export interface AssistantCodeCompletionOptions__Output {
+  'contextId': (string);
+}
